979a455a661083a9f3db06a5e6572f9abe3f8d08
eagle <aparokshambhargav@gmail.com>
2024-09-25 22:02:09 +0530

    feat: add trackedAccount schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import { OAuthData, Tweet } from "../types";
+import { OAuthData, TrackedAccount, Tweet, TweetQueue } from "../types";
 
 // OAuthData Schema
 const oauthDataSchema = new Schema<OAuthData>({
@@ -14,9 +14,35 @@ const tweetSchema = new Schema<Tweet>({
   sent: { type: Boolean, default: false },
 });
 
+const trackedAccountsSchema = new Schema<TrackedAccount>({
+  address: { type: String, required: true },
+  ensName: { type: String, required: true },
+  listTokenId: { type: String, required: true },
+  followedAccounts: { type: [String], default: [] },
+  lastChecked: { type: Date, default: Date.now },
+});
+
+const tweetQueueSchema = new Schema<TweetQueue>({
+  address1: { type: String, required: true },
+  ensName1: { type: String, required: true },
+  address2: { type: String, required: true },
+  ensName2: { type: String, required: true },
+  action: { type: String, required: true },
+});
+
 // Models
 export const OAuthDataModel = mongoose.model<OAuthData>(
   "OAuthData",
   oauthDataSchema
 );
 export const TweetModel = mongoose.model<Tweet>("Tweet", tweetSchema);
+
+export const TrackedAccountModel = mongoose.model<TrackedAccount>(
+  "TrackedAccount",
+  trackedAccountsSchema
+);
+
+export const TweetQueueModel = mongoose.model<TweetQueue>(
+  "TweetQueue",
+  tweetQueueSchema
+);
--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,21 @@ export interface Tweet {
   content: string;
   sent: boolean;
 }
+
+export interface TrackedAccount {
+  address: string;
+  ensName: string;
+  listTokenId: string;
+  followedAccounts: string[];
+  lastChecked: Date;
+}
+
+export interface TweetQueue {
+  address1: string;
+  ensName1: string;
+  address2: string;
+  ensName2: string;
+  action: UserAction;
+}
+
+export type UserAction = "follow" | "unfollow" | "block";
