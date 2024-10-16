ce0a67f4f04d4c6c5ce9b7c50c6eb232b90e3c2c
eagle <aparokshambhargav@gmail.com>
2024-10-16 18:58:25 +0530

    feat: add sendTweets functionality

--- src/services/tweetService.ts
+++ src/services/tweetService.ts
@@ -1,13 +1,15 @@
 import { getNextTweet } from "../database/dbFunctions";
+import { TweetQueueModel } from "../database/schema";
+import { TweetQueue } from "../types";
 import { userClient } from "./twitterClient";
 
-export async function sendTweet(): Promise<void> {
+export async function sendTweet(tweetData: TweetQueue): Promise<void> {
   if (!userClient) {
     console.error("User client not initialized. Please authenticate first.");
     return;
   }
 
-  const tweetContent = await getNextTweet();
+  const tweetContent = `${tweetData.ensName1} has ${tweetData.action}ed ${tweetData.ensName2}`;
   if (tweetContent) {
     try {
       console.log(`Attempting to send tweet: ${tweetContent}`);
@@ -19,4 +21,20 @@ export async function sendTweet(): Promise<void> {
   } else {
     console.log("No more tweets to send.");
   }
+}
+
+export async function sendTweets(): Promise<void> {
+  const tweetQueueData = await TweetQueueModel.find({});
+
+  // loop through tweetQueueData and send tweet & if tweet is sent, remove it from db
+  tweetQueueData.forEach(async (tweet) => {
+    try {
+      setTimeout(async () => {
+        await sendTweet(tweet);
+      }, 2000);
+      await TweetQueueModel.deleteOne({ _id: tweet._id });
+    } catch (error) {
+      console.error("Error sending tweet:", error, tweet._id);
+    }
+  });
 }
